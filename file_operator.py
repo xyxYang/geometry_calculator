fd0ac3060cd368a5f4a7a1d2611abea034ff9913
xyx_yang@foxmail.com <xyx_yang@foxmail.com>
2019-11-10 17:08:15 +0800

    基础文件读写代码

---
+++ file_operator.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2019/11/10 4:36 PM 
+# @Author : yangyuxin
+# @File : file_operator.py
+
+
+import os
+import sys
+import ogr
+
+
+def create_miffile(file_path, fieldDef_list) :
+    ds_file = None
+    ds_driver = None
+    lyr_file = None
+    lyr_name = ""
+    pos = file_path.rfind('.')
+    if pos != -1 :
+        lyr_name = file_path[0 : pos]
+    pos = lyr_name.rfind(os.sep)
+    if pos != -1 :
+        lyr_name = lyr_name[pos + 1 : len(lyr_name)]
+    ds_driver = ogr.GetDriverByName("MapInfo File")
+    if ds_driver is None :
+        return ds_file
+    ds_file = ds_driver.CreateDataSource(file_path)
+    if ds_file is None :
+        return ds_file
+    lyr_file = ds_file.CreateLayer(lyr_name)
+    if lyr_file is None :
+        ds_file = None
+        return ds_file
+    for fieldDef in fieldDef_list:
+        lyr_file.CreateField(fieldDef)
+    return ds_file
+
+
+class FileReader(object):
+    def __init__(self, file_path):
+        self.ds_file = ogr.Open(file_path)
+        if self.ds_file:
+            self.lyr_file = self.ds_file.GetLayerByIndex(0)
+
+    def is_valid(self):
+        return self.ds_file and self.lyr_file
+
+    def get_lyr_file(self):
+        if not self.is_valid():
+            return None
+
+        self.lyr_file.ResetReading()
+        return self.lyr_file
+
+    def get_field_def_list(self):
+        if not self.is_valid():
+            return None
+
+        field_def_list = []
+        feature = self.lyr_file.GetNextFeature()
+        for i in range(feature.GetFieldCount()):
+            field_def_list.append(feature.GetFieldDefnRef(i))
+        return field_def_list
+
+
+class FileWriter(object):
+    def __init__(self, file_path, field_def_list):
+        self.ds_file = create_miffile(file_path, field_def_list)
+        if self.ds_file:
+            self.lyr_file = self.ds_file.GetLayerByIndex(0)
+
+    def is_valid(self):
+        return self.ds_file and self.lyr_file
+
+    def write_feature(self, feature):
+        if not self.is_valid():
+            return False
+        self.lyr_file.CreateFeature(feature)
+        return True
+
+
+
